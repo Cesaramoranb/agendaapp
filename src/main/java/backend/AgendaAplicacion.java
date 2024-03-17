e8ee8b91ff8a2c516a6a7785aa4b2e87f7e14496
xcamb <xcamb@DESKTOP-UQHBU89.lan>
2024-03-17 16:38:52 +0100

    primer commit

---
+++ src/main/java/backend/AgendaAplicacion.java
@@ -0,0 +1,13 @@
+package backend;
+
+import frontend.Login;
+
+public class AgendaAplicacion {
+
+    public static void main(String[] args) {
+        Login screen = new Login();
+        screen.setVisible(true);
+        screen.setLocationRelativeTo(null);
+        
+    }
+}
