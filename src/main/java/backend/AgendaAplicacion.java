414c17a2c0cbe6ac43eef3df850e2654c9e72972
xcamb <xcamb@DESKTOP-UQHBU89.lan>
2024-03-28 00:48:54 +0100

    implementacion parcial de ver eventos

--- src/main/java/backend/AgendaAplicacion.java
+++ src/main/java/backend/AgendaAplicacion.java
@@ -1,16 +1,14 @@
 package backend;
 
 import frontend.*;
+
  // Ejecutable 
 public class AgendaAplicacion {
 
     public static void main(String[] args) {
-        Login screen = new Login();
-        screen.setVisible(true);
-        screen.setLocationRelativeTo(null);
-        //App test = new App();
-        //test.setVisible(true);
-        //test.setLocationRelativeTo(null);
-        
+        Login apv = new Login();
+        apv.setVisible(true);
+        apv.setLocationRelativeTo(null);
+       
     }
 }
